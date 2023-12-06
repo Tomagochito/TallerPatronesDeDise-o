5b408c7cb1cb6fbc1c9235c55bb0beb7f68b7d84
Nathaly Crespo <npcrespo@espol.edu.ec>
2023-12-06 06:10:29 -0500

    Factory method

---
+++ Taller07/src/main/java/com/mycompany/taller07/FactoryMethod/Celular.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller07.FactoryMethod;
+
+/**
+ *
+ * @author nathaly
+ */
+public class Celular implements Producto {
+
+    @Override
+    public void caracteristicas() {
+        System.out.println("Caracteristicas dell celular");
+    }
+    
+}
---
+++ Taller07/src/main/java/com/mycompany/taller07/FactoryMethod/CelularFactory.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller07.FactoryMethod;
+
+/**
+ *
+ * @author nathaly
+ */
+public class CelularFactory extends ProductFactory{
+
+    @Override
+    public Producto createProducto() {
+        return new Celular();
+    }
+    
+    
+    
+}
